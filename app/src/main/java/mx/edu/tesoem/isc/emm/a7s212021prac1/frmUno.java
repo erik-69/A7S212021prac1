303affe73facba667206b0f8773af23cde174d25
Erik Maya Mondragón <71298062+erik-69@users.noreply.github.com>
2020-09-28 19:43:37 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/emm/a7s212021prac1/frmUno.java
@@ -0,0 +1,17 @@
+package mx.edu.tesoem.isc.emm.a7s212021prac1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class frmUno extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_uno);
+    }
+}
